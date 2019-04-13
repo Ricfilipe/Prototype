26624a8f5a22c33ed102770d5bea24cb59e366c4
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2019-04-13 18:49:25 +0100

    close #3
    Falta apenas mudar usar UnitStats
    #1, para vida

---
+++ Assets/Our Scripts/UI/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject unit;
+    private UnitStats myUnitStats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myUnitStats = unit.GetComponent<UnitStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<RectTransform>().localScale = new Vector3(5/10f,1,1);
+        Vector3 pos = unit.transform.position;
+         gameObject.transform.parent.parent.position= new Vector3(pos.x, pos.y + 2, pos.z);
+     
+
+
+    }
+}
