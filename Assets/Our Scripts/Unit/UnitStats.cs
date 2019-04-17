52c89e4570a564b78c8299e468564f0ef4c37a24
Nuno <nuno.bras.pinhao@tecnico.ulisboa.pt>
2019-04-17 17:36:43 +0100

    unitstats

--- Assets/Our Scripts/Unit/UnitStats.cs
+++ Assets/Our Scripts/Unit/UnitStats.cs
@@ -8,11 +8,120 @@ public class UnitStats : MonoBehaviour
     public float speed,range,size;
     [HideInInspector]
     public int MaxHP, HP,AD;
+   	
+   	private static int InfantryMaxHP, InfantryAD, ArcherMaxHP, ArcherAD;
+
+    public enum Team
+    {
+    	England,
+    	France,
+    };
+
+    public enum Troops
+    {
+    	Archer,
+    	Infantry,
+    	Cavalry,
+    	CrossBowMan,
+    	King,
+    };
+
+ 	public Team team;
+    public Troops troop;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    	switch(team)
+    	{
+    		case Team.England:
+    			switch(troop)
+    			{
+    				case Troops.Infantry:
+    					this.speed=4;
+	    				this.range=1.5f;
+	    				this.size=5;
+	    				InfantryMaxHP=6;
+	    				this.HP=6;
+	    				InfantryAD=3;
+	    				break;
+
+	    			case Troops.Archer:
+	    				this.speed=3;
+	    				this.range=10;
+	    				this.size=4;
+	    				ArcherMaxHP=4;
+	    				this.HP=4;
+	    				ArcherAD=2;
+	    				break;
+
+	    			case Troops.King:
+	    				this.speed=6;
+	    				this.range=2;
+	    				this.size=9;
+	    				this.MaxHP=13;
+	    				this.HP=13;
+	    				this.AD=3;
+	    				break;
+    			}
+    			break;	   		
+
+    		case Team.France:
+    			switch(troop)
+    			{
+    				case Troops.Archer:
+	    				this.speed=3;
+	    				this.range=9;
+	    				this.size=4;
+	    				this.MaxHP=3;
+	    				this.HP=3;
+	    				this.AD=2;
+	    				break;
+    				
+    				case Troops.Infantry:
+    					this.speed=4;
+	    				this.range=1.5f;
+	    				this.size=5;
+	    				this.MaxHP=5;
+	    				this.HP=5;
+	    				this.AD=2;
+	    				break;
+
+	    			case Troops.Cavalry:
+	    				this.speed=5;
+	    				this.range=2;
+	    				this.size=8;
+	    				this.MaxHP=5;
+	    				this.HP=5;
+	    				this.AD=2;
+	    				break;
+
+	    			case Troops.CrossBowMan:
+	    				this.speed=4;
+	    				this.range=7;
+	    				this.size=4;
+	    				this.MaxHP=3;
+	    				this.HP=3;
+	    				this.AD=2;
+	    				break;
+
+	    			case Troops.King:
+	    				this.speed=6;
+	    				this.range=2;
+	    				this.size=9;
+	    				this.MaxHP=9;
+	    				this.HP=9;
+	    				this.AD=2;
+	    				break;
+    			}
+    			break;
+
+    	}
+
     }
+
+
+
+
 
 }
