02fb6a88ae194aed1d664242aa488e39a3597914
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2019-04-18 00:46:05 +0100

    script

---
+++ Assets/Our Scripts/UI/groupHUD.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groupHUD : MonoBehaviour
+{
+
+    public GameObject[] buttons = new GameObject[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject but in buttons) {
+            but.active = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
