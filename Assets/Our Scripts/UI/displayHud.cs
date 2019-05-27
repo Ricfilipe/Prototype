819facf591c5ca77e59db46d8fefe020a4a79ba3
Ricfilipe <ricardo.l.filipe@tecnico.ulisboa.pt>
2019-05-27 23:54:36 +0100

    fix menu

--- Assets/Our Scripts/UI/displayHud.cs
+++ Assets/Our Scripts/UI/displayHud.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class displayHud : MonoBehaviour
 {
     public GameObject textObject, imageObject, descriptionObject,gameManager;
-
+    private Sprite[] images = new Sprite[3];
 
     private Text name, description;
     private GameManager gm;
@@ -17,7 +17,10 @@ public class displayHud : MonoBehaviour
         this.name = textObject.GetComponent<Text>();
         this.description = descriptionObject.GetComponent<Text>();
         this.gm = gameManager.GetComponent<GameManager>();
-           
+        images[0] = Resources.Load<Sprite>("hhhh");
+        images[1] = Resources.Load<Sprite>("Infantaria");
+        images[2] = Resources.Load<Sprite>("Infantaria");
+        imageObject.GetComponent<Image>().sprite = images[0];
     }
 
     // Update is called once per frame
@@ -29,28 +32,38 @@ public class displayHud : MonoBehaviour
         {
             this.name.text = "King Henry V";
             writeDescriptionSing(gm.King.GetComponentInParent<UnitStats>());
+            imageObject.GetComponent<Image>().sprite = images[0];
         }
         else if (gm.King == null && gm.archerSelected.Count == 1 && gm.knightSelected.Count == 0)
         {
             this.name.text = "Archer";
             writeDescriptionSing(gm.archerSelected[0].GetComponent<UnitStats>());
+            imageObject.GetComponent<Image>().sprite = images[2];
         }
         else if (gm.King == null && gm.archerSelected.Count == 0 && gm.knightSelected.Count == 1)
         { 
             this.name.text = "Men-At-Arms";
             writeDescriptionSing(gm.knightSelected[0].GetComponent<UnitStats>());
+            imageObject.GetComponent<Image>().sprite = images[1];
         }
         else {
-            if (gm.King != null) {
-                this.description.text = this.description.text + "King: 1\n";
-            }
-            if(gm.archerSelected.Count >= 1) {
-                this.description.text = this.description.text + "Archer: "+ gm.archerSelected.Count + "\n";
+            if (gm.archerSelected.Count >= 1)
+            {
+                this.description.text = this.description.text + "Archer: " + gm.archerSelected.Count + "\n";
+                imageObject.GetComponent<Image>().sprite = images[2];
             }
             if (gm.knightSelected.Count >= 1)
             {
                 this.description.text = this.description.text + "Men-At-Arms: " + gm.knightSelected.Count + "\n";
+                imageObject.GetComponent<Image>().sprite = images[1];
             }
+
+            if (gm.King != null)
+            {
+                this.description.text = this.description.text + "King: 1\n";
+                imageObject.GetComponent<Image>().sprite = images[0];
+            }
+
 
         }
     }
